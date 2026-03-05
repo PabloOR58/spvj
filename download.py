2a2ee413cdc983ccdd4d6e00c1b8f135a05aa308
jrxschz <cesterin30@icloud.com>
2026-03-05 17:43:01 +0100

    comit

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ def generar_listado():
     url_top = "https://api.steampowered.com/ISteamChartsService/GetGamesByConcurrentPlayers/v1/"
     
     try:
-        print("1. Conectando con Steam...")
+       
         respuesta = requests.get(url_top)
         datos = respuesta.json()
         juegos = datos.get('response', {}).get('ranks', [])
@@ -13,16 +13,16 @@ def generar_listado():
             print("No hay datos.")
             return
 
-        print("2. Creando el archivo...")
+       
         with open("listado_juegos.txt", "w", encoding="utf-8") as fichero:
-            fichero.write("TOP 10 JUEGOS MAS JUGADOS\n")
-            fichero.write("-" * 30 + "\n")
+            fichero.write("Top 10 juegos mas jugados en el momento:\n")
+            
 
             for i in range(10):
                 appid = juegos[i].get('appid')
                 jugadores = juegos[i].get('concurrent_in_game', 0)
 
-                # Buscamos el nombre del juego
+                
                 url_n = f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=basic"
                 try:
                     res_n = requests.get(url_n).json()
@@ -30,12 +30,12 @@ def generar_listado():
                 except:
                     nombre = f"ID: {appid}"
 
-                # Escribimos la línea (CUIDADO CON LOS PARÉNTESIS AQUÍ)
+               
                 linea = f"{i+1}. {nombre} | {jugadores:,} jugadores\n"
                 fichero.write(linea)
                 print(f"OK: {nombre}")
 
-        print("\n¡PROCESO COMPLETADO! Mira tu archivo listado_juegos.txt")
+        print("\n Proceso completado.")
 
     except Exception as e:
         print(f"Error: {e}")
